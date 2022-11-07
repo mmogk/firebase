e0b944c4c909a6cad4a6dcf8f04f8b4e99fed90f
mmogk <mmogk@my.bcit>
2022-11-06 18:22:34 -0800

    commit

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,15 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_apiKey,
-    authDomain: process.env.NEXT_PUBLIC_authDomain,
-    projectId: process.env.NEXT_PUBLIC_projectId,
-    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-    appId: process.env.NEXT_PUBLIC_appId
-}; 
+  apiKey: "AIzaSyC4kKV5LOpqJWZJKXfRpl9atF9Mi1dfS2Q",
+  authDomain: "login-with-firebase-1552e.firebaseapp.com",
+  projectId: "login-with-firebase-1552e",
+  storageBucket: "login-with-firebase-1552e.appspot.com",
+  messagingSenderId: "524585752816",
+  appId: "1:524585752816:web:8327369701ce5a2b964a35"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
